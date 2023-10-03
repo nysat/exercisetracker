76176dfe08c98c71cff39620106b6d7f79774d5e
Brian A <brian.ahlers@gmail.com>
2023-10-03 01:28:06 -0700

    created server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express'); // import express
+const mysql = require('mysql'); // import mysql
+
+const app = express();  // create express app   
+
+// create connection to database
+const connection = mysql.createConnection({
+    
+});
