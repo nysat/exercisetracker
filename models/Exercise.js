398e83411c63f859a5a19d612e8993e940bacaeb
nysat <nysa-cere3@hotmail.com>
2023-10-05 22:50:10 -0400

    added more comments and created some routes folders

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,4 +1,8 @@
 // storeinformation about exercise
+//models are going to be a way for use to create "templates" for our data so if we want to give the user 
+//the ability to create a workout we can use the model to create a template for the workout and tell it what its going to populate as
+// like name, reps, sets, weight, rpe, bodyweight, comments, etc. but in json format 
+
 
 // exercise name
 // date
--- models/User.js
+++ models/User.js
@@ -1,9 +1,13 @@
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
 
-// user needs to be able to tie exercises and dates to their client account
+//remember to require bcrypt
+//remember that "model" and "datatypes" are from sequelize its a package that sequelize lets usq use
 class User extends Model {
 
 }
 
+
+
+// user needs to be able to tie exercises and dates to their client account
 // basic information username/password
