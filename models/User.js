f3cf11e4f729aa647457fdfa6a198f384975ea4c
nysat <nysa-cere3@hotmail.com>
2023-10-05 20:47:56 -0400

    adding details and comments of what we are going to need

--- models/User.js
+++ models/User.js
@@ -1,2 +1,9 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
 // user needs to be able to tie exercises and dates to their client account
+class User extends Model {
+
+}
+
 // basic information username/password
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+require('dotenv').config(); // import dotenv package
 const express = require('express'); // import express
+const sequelize = require('./config/connection');
 const mysql = require('mysql'); // import mysql  ////this is supposed to say 'mysql2'
 
+
+//Import Models to sync them with the database
+
+
 const app = express();  // create express app   
+const PORT = process.env.PORT || 3001; // set port to 3001//we still have to set this up in .env file 
+
+//middleware 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Import connection object for sequelize
 const sequelize = require('./config/connection');
