72e9b74508105605a635fbb249949817e08378a9
Caleb B <calebabaca@gmail.com>
2023-10-05 17:01:45 -0700

    added nav bars to new and calander

--- work.views/calendar/calendar.js
+++ work.views/calendar/calendar.js
@@ -45,3 +45,22 @@ prevNextIcon.forEach(icon => { // getting prev and next icons
         renderCalendar(); // calling renderCalendar function
     });
 });
+
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'your-hostname',
+    port: 'your-port',
+    user: 'your-username',
+    password: 'your-password',
+    database: 'your-database-name'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to the database as id ' + connection.threadId);
+});
