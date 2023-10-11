e4ec8500fa1282a2430fa5bef5ff2a9e4fae8713
Brian A <brian.ahlers@gmail.com>
2023-10-10 22:13:14 -0700

    added more code to newworkout js

--- work.views/newworkout/newworkout.js
+++ work.views/newworkout/newworkout.js
@@ -31,12 +31,32 @@ function logExercise() {
         return;
     }
 
-    console.log({
+    const data = {
         exercise,
         sets,
         reps,
         weight,
         rpe,
         comments
+    };
+
+    fetch('/api/exercises', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Exercise logged successfully:', data);
+    })
+    .catch(error => {
+        console.error('Error logging exercise:', error);
     });
 }
