b226a53c32c5aead92adba6cd744f1ace0ebb47c
nysat <nysa-cere3@hotmail.com>
2023-10-12 18:55:54 -0400

    cleaned up code

--- routes/api/exerciseRoutes.js
+++ routes/api/exerciseRoutes.js
@@ -1,6 +1,23 @@
 const router = require('express').Router();
 const { Exercise } = require('../../models/Exercise');
 
+router.get('/', async (req, res) => {
+    Exercise.findAll().then((exerciseData) => {
+        res.json(exerciseData);
+    }
+    ).catch((err) => {
+        res.json(err);
+    }); 
+});
 
-
+router.put('/:id', async (req, res) => {
+    Exercise.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(updatedExercise => {
+        res.json(updatedExercise)}
+        )
+});
 module.exports = router;
