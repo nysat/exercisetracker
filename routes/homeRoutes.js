99043a073b8a10095fdd39c539a89a2c06f805df
nysat <nysa-cere3@hotmail.com>
2023-10-12 21:00:34 -0400

    added a homeRoutes route for the logged in user and the withAuth funciton to protect the route

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,23 +1,51 @@
 const router = require('express').Router();
 const { User, Exercise } = require('../models');
-
-//this is where we will have the login page     
+const withAuth = (`../utils/auth`);
+//this is where we will have the login page    
 router.get('/', function (req, res) {
-    res.render('login',);
+    res.render('login', {});
   });
+//serialize data for handlebars so it can read it
 
-router.get('/home', function (req, res) {
-    res.render('home',);
-  }
-);
-//serialize data for handlebars so it can read it 
 
 //pass serialized data into a template
 
+
 //we can add a .get route to get all the exercises for a user and render them on the page
 
-//if  the user is already logged in we can redirect them to the home page or direct them to a different route 
+
+//use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => { //you guys can change the /profile to whatever matches to the handlebars page that will have
+  //the logged in user's profile
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Exercise }],
+    });
 
 
+    const user = userData.get({ plain: true });
 
+
+    res.render('profile', {  //you probably need to change this to whatever the handlebars page is called
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+
+  res.render('login');
+});
 module.exports = router;
