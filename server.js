840bb47f443655fd267b148303c2e3134abe58ee
nysat <nysa-cere3@hotmail.com>
2023-10-13 21:24:30 -0400

    added sessions and created a couple routes

--- server.js
+++ server.js
@@ -2,17 +2,28 @@ require('dotenv').config(); // import dotenv package
 const express = require('express'); // import express]
 const exhbs = require('express-handlebars'); // import handlebars
 const Sequelize = require('sequelize'); // import sequelize package
-const hbs = exhbs.create();
+const hbs = exhbs.create({});
 const sequelize = require('./config/connection');//Import connection object for sequelize
 const path = require('path');
+const session = require('express-session'); // import express-session
+const withAuth = require('./utils/auth'); // import auth middleware
 
 //Import Models to sync them with the database
 const {User} = require('./models'); 
 const {Exercise} = require('./models'); //ylcbranch
 
-
-const app = express();  // create express app   
+// create express app  
+const app = express();   
 const PORT = process.env.PORT || 3001; // set port to what we have in the .env file or 3001
+
+//set up sessions
+const sess = {
+    secret: process.env.SECRET,
+    resave: false,
+    saveUnitialized: true,
+}
+
+app.use(session(sess));
 
 //middleware 
 app.use(express.json());
@@ -23,7 +34,10 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars'); 
 app.set('views', __dirname + '/views');
 
-app.use(express.static(path.join(__dirname, 'public')));//this is to serve static files like css and js files
+app.use(express.static(path.join(__dirname, 'public')));//this is to serve static files like css and js files//this is only
+//going to work if we have apublic folder in the root directory with the css files we need which we should do to maintain the mvc structure
+
+app.use(withAuth); // use auth middleware
 app.use(require('./routes')); // import routes
 
 // create connection to database
--- utils/auth.js
+++ utils/auth.js
@@ -6,5 +6,7 @@ const withAuth = (req, res, next) => {// If the user is not logged in, redirect 
       next();
     }
   };
+  //this is middleware so this is a point that your code goes through before it gets to the routes and since we have this here 
+  //its to verify that the user is logged in and has a session before anything else
  
   module.exports = withAuth;
