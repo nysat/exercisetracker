7b91851596932da0ed8a539f52113cc6a255be36
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-10-15 17:14:33 -0400

    seeds folder created

---
+++ seeds/seeds.js
@@ -0,0 +1,25 @@
+const sequelize = require('.../config/connection');
+const { User, Exercise } = require('../models');
+
+const userData = require('./userData.json');
+const exerciseData = require('./exerciseData.json');
+
+const seedDatabase = async()=>{
+    await sequelize.sync({force: true});
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for(const exercise of exerciseData){
+        await Exercise.create({
+            ...exercise,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+    }
+
+    process.exit(0);
+}
+
+seedDatabase();
