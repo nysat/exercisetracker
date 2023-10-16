4e827be89b7d76cedc3cd62b8a737ee50ce61260
nysat <nysa-cere3@hotmail.com>
2023-10-16 17:15:45 -0400

    changed environment variables

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.MYSQL_URI) {
+  sequelize = new Sequelize(process.env.MYSQL_URI);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
