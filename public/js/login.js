949f2c61fa789d21628ccc18156e7b077fb11ca4
Brian A <brian.ahlers@gmail.com>
2023-10-15 18:57:06 -0700

    newworkout changed exercise to open input and corrected route errors in loginjs

--- public/js/login.js
+++ public/js/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async(event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/profile');
+            document.location.replace('/home');
         } else {
             alert(response.statusText);
         }
@@ -36,7 +36,7 @@ const signupFormHandler = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/profile');
+            document.location.replace('/home');
         } else {
             alert(response.statusText);
         }
