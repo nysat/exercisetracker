f19918f487466219d7105350cf2399797bc626d5
Brian A <brian.ahlers@gmail.com>
2023-10-15 17:52:43 -0700

    adding script links to handlebars views, testing error

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async(event) => {
     ('#password-login').value.trim();
 
     if (email && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('../../routes/api/userRoutes.js', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
             headers: { 'Content-Type': 'application/json'}
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,4 +1,4 @@
-const sequelize = require('.../config/connection');
+const sequelize = require('../config/connection');
 const { User, Exercise } = require('../models');
 
 const userData = require('./userData.json');
