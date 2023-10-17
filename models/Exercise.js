1fc526233fe24508f5605738f1a27e2cb54df7a3
Brian A <brian.ahlers@gmail.com>
2023-10-16 18:11:05 -0700

    working to implement date

--- models/Exercise.js
+++ models/Exercise.js
@@ -20,10 +20,10 @@ Exercise.init({
         type: DataTypes.STRING,   //this is where you define the data type for each column in the table
         allowNull: true,
     },
-    // date: {// date
-    //     type: DataTypes.DATE,
-    //     allowNull: false,
-    // },
+    date: {// date
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
     sets: {// sets
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -58,7 +58,6 @@ Exercise.init({
     },
     {//In this second object literal youre going to define any configuration for the table and make a connection to the database
         sequelize,
-        // timestamps: false,
         freezeTableName: true,
         underscored: true,
         modelName: 'exercise',
