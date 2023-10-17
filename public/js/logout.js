a4e464cdee78bf1f42a4b275a37520b09e87789a
Caleb B <calebabaca@gmail.com>
2023-10-16 23:52:53 -0700

    revamping logout.1

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,23 +1,14 @@
-logout.js
-
-const logoutButton = document.getElementById('logout');
-
-logoutButton.addEventListener('click', async () => {
-    try {
-      const response = await fetch('/api/users/logout', {
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        window.location.href = '/home';
-      } else {
-        throw new Error('Failed to logout');
-      }
-    } catch (err) {
-      console.error(err);
-      alert('Failed to logout');
-    }
-  });
+        headers: { 'Content-Type': 'application/json'},
+    });
 
-// document.querySelector('#logout').addEventListener('click', logout);
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
